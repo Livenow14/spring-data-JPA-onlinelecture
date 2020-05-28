5c95a4dac7006e1b08b5699b13d59ea5d358f50c
Livenow14 <xowjd41@naver.com>
2020-05-28 20:53:55 +0900

    initialize commit

---
+++ data-jpa/src/main/java/com/livenow/datajpa/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.livenow.datajpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ data-jpa/src/test/java/com/livenow/datajpa/controller/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.livenow.datajpa.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void hello() {
+    }
+}
