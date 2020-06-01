15c776c830b84b0ae27d6cf7b810644429fca794
Livenow14 <xowjd41@naver.com>
2020-06-01 15:42:52 +0900

    공통 인터페이스 기능_ 순수 JPA 기반 Repository 만들기(test code 포함)

--- data-jpa/src/main/java/com/livenow/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/com/livenow/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -20,5 +22,24 @@ public class MemberJpaRepository {
 
     public Member find(Long id){
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+    
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }
--- data-jpa/src/test/java/com/livenow/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/com/livenow/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +34,38 @@ class MemberJpaRepositoryTest {
         assertThat(saveMember.getId()).isEqualTo(findMember.getId());
         assertThat(saveMember.getUserName()).isEqualTo(findMember.getUserName());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @DisplayName("crud 조회")
+    @Test
+    public void basicCrud() throws Exception{
+        //given
+        Member member1= new Member("member1");
+        Member member2= new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //when
+        Member findMember1= memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2= memberJpaRepository.findById(member2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 
 }
