a45f08f0847a5c8319d15ba6b696b6c17c914653
Livenow14 <xowjd41@naver.com>
2020-06-01 17:12:45 +0900

    공통 인터페이스 기능_ 공통 인터페이스분석(data jpa의 마법?을 알기전 과정)

--- data-jpa/src/test/java/com/livenow/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/com/livenow/datajpa/repository/MemberRepositoryTest.java
@@ -45,29 +45,29 @@ class MemberRepositoryTest {
         //given
         Member member1= new Member("member1");
         Member member2= new Member("member2");
-        memberRepository .save(member1);
-        memberRepository .save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
         //when
-        Member findMember1= memberRepository .findById(member1.getId()).get();
-        Member findMember2= memberRepository .findById(member2.getId()).get();
+        Member findMember1= memberRepository.findById(member1.getId()).get();
+        Member findMember2= memberRepository.findById(member2.getId()).get();
 
         //then
         assertThat(findMember1).isEqualTo(member1);
         assertThat(findMember2).isEqualTo(member2);
 
         //리스트 조회
-        List<Member> all = memberRepository .findAll();
+        List<Member> all = memberRepository.findAll();
         assertThat(all.size()).isEqualTo(2);
 
         //카운트 검증
-        long count = memberRepository .count();
+        long count = memberRepository.count();
         assertThat(count).isEqualTo(2);
 
-        memberRepository .delete(member1);
-        memberRepository .delete(member2);
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
 
-        long deletedCount = memberRepository .count();
+        long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
 
