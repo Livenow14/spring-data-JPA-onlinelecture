63982a353a12b09e3900baf1546738222601a1e4
Livenow14 <xowjd41@naver.com>
2020-07-26 16:01:07 +0900

    나머지 기능들_Query By Example
    이것또한 Join문제로 실무에서 잘 쓰이지않음

--- data-jpa/src/main/java/com/livenow/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/com/livenow/datajpa/repository/MemberRepository.java
@@ -167,5 +167,33 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
      * 그냥 쓰는 방법만 알고 넘어가는게 좋음 .
      */
 
+    /**
+     * Query By Example
+     * 여기서 문제는 join 에서 잘 안됨 (inner join만 됨 )
+     * 복잡한 join이 되지 않아 실무에서 잘 안씀
+     *
+     * Probe: 필드에 데이터가 있는 실제 도메인 객체
+     * ExampleMatcher: 특정 필드를 일치시키는 상세한 정보 제공, 재사용 가능
+     * Example: Probe와 ExampleMatcher로 구성, 쿼리를 생성하는데 사용
+     *
+     * 장점
+     * 동적 쿼리를 편리하게 처리
+     * 도메인 객체를 그대로 사용
+     * 데이터 저장소를 RDB에서 NOSQL로 변경해도 코드 변경이 없게 추상화 되어 있음
+     * 스프링 데이터 JPA JpaRepository 인터페이스에 이미 포함
+     *
+     * 단점
+     * 조인은 가능하지만 내부 조인(INNER JOIN)만 가능함 외부 조인(LEFT JOIN) 안됨
+     * 다음과 같은 중첩 제약조건 안됨
+     * firstname = ?0 or (firstname = ?1 and lastname = ?2)
+     * 매칭 조건이 매우 단순함
+     * 문자는 starts/contains/ends/regex
+     * 다른 속성은 정확한 매칭( = )만 지원
+     * 
+     * 정리
+     * 실무에서 사용하기에는 매칭 조건이 너무 단순하고, LEFT 조인이 안됨
+     * 실무에서는 QueryDSL을 사용하자
+     * */
+
 
 }
--- data-jpa/src/test/java/com/livenow/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/com/livenow/datajpa/repository/MemberRepositoryTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -410,8 +408,63 @@ class MemberRepositoryTest {
         List<Member> findAll = memberRepository.findAll();
 
         assertThat(memberCustom.get(0).getAge()).isEqualTo(findAll.get(0).getAge());
+    }
 
+    /**
+     * Query By Example
+     * 여기서 문제는 join 에서 잘 안됨 (inner join만 됨 )
+     * 복잡한 join이 되지 않아 실무에서 잘 안씀
+     *
+     * Probe: 필드에 데이터가 있는 실제 도메인 객체
+     * ExampleMatcher: 특정 필드를 일치시키는 상세한 정보 제공, 재사용 가능
+     * Example: Probe와 ExampleMatcher로 구성, 쿼리를 생성하는데 사용
+     *
+     * 장점
+     * 동적 쿼리를 편리하게 처리
+     * 도메인 객체를 그대로 사용
+     * 데이터 저장소를 RDB에서 NOSQL로 변경해도 코드 변경이 없게 추상화 되어 있음
+     * 스프링 데이터 JPA JpaRepository 인터페이스에 이미 포함
+     *
+     * 단점
+     * 조인은 가능하지만 내부 조인(INNER JOIN)만 가능함 외부 조인(LEFT JOIN) 안됨
+     * 다음과 같은 중첩 제약조건 안됨
+     * firstname = ?0 or (firstname = ?1 and lastname = ?2)
+     * 매칭 조건이 매우 단순함
+     * 문자는 starts/contains/ends/regex
+     * 다른 속성은 정확한 매칭( = )만 지원
+     *
+     * 정리
+     * 실무에서 사용하기에는 매칭 조건이 너무 단순하고, LEFT 조인이 안됨
+     * 실무에서는 QueryDSL을 사용하자
+     * */
+    @Test
+    public void queryByExample(){
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
 
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
 
+        em.flush();
+        em.clear();
+
+        //when
+        //Probe
+        Member member = new Member("m1");
+        Team teamA1 = new Team("teamA");
+        member.setTeam(teamA1);
+
+        /**
+         * null 일때 넣어줄걸
+         */
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("age");
+
+        Example<Member> example = Example.of(member, matcher);
+        List<Member> result = memberRepository.findAll(example);
+        assertThat(result.get(0).getUserName()).isEqualTo("m1");
     }
 }
